819c71e7066b8ef54c968d437de726daca0bbba6
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-11-16 16:48:30 +0800

    feat: add index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,10 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { BookGridList } from "~/components/book-grid-list/book-grid-list";
-import { ThemeToggle } from "~/components/theme-toggle/theme-toggle";
 
 export default component$(() => {
   return (
     <>
-      <div class="">
-        <h1>Hi 👋</h1>
-        <p>
-          Can't wait to see what you build with qwik!
-          <br />
-          Happy coding.
-        </p>
-        <button class="btn">123</button>
-      </div>
-      <ThemeToggle />
       <BookGridList />
     </>
   );
